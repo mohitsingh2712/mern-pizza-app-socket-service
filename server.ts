98f8f3eb00c81b8b63380f092d5be6c86c4cf581
mo14337 <mohitsingh14336@gmail.com>
2025-04-25 20:03:13 +0530

    configured ws server

--- server.ts
+++ server.ts
@@ -1,15 +1,31 @@
 import logger from "./src/config/logger";
 import { createMessageBroker } from "./src/factories/broker-factory";
 import { MessageBroker } from "./src/types/broker";
+import ws from "./src/socket";
+import config from "config";
 
 const startServer = async () => {
   let broker: MessageBroker | null = null;
+  const port = config.get("server.port") || 3000;
   try {
     broker = createMessageBroker();
     await broker.connectConsumer();
     await broker.consumeMessage(["order"], false);
+    ws.wsServer
+      .listen(port, () => {
+        logger.info(`WebSocket server is running on port ${port}`);
+      })
+      .on("error", (err) => {
+        console.log(err);
+        logger.error("Error happened: ", err.message);
+        process.exit(1);
+      });
   } catch (err) {
+    console.log(err);
     logger.error("Error happened: ", err.message);
+    if (broker) {
+      await broker.disconnectConsumer();
+    }
     process.exit(1);
   }
 };
