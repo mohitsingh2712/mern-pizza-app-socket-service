8d4fe50b81cc3376a0c19b2575bf87ff40beb9d0
mo14337 <mohitsingh14336@gmail.com>
2025-04-25 19:49:51 +0530

    add websocket server

---
+++ src/socket.ts
@@ -0,0 +1,25 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+import config from "config";
+
+const wsServer = createServer();
+const frontendUrl = config.get("frontend.url");
+const io = new Server(wsServer, {
+  cors: {
+    origin: frontendUrl as string,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected", socket.id);
+  socket.on("join", (data) => {
+    socket.join(String(data.tenantId));
+    socket.emit("join", { roomId: String(data.tenantId) });
+  });
+});
+
+export default {
+  wsServer,
+  io,
+};
